e8a73bc9e190aa83f48d714e73ac6efcda90123b
Eduardo Horstmann <eduardohorstmann@hotmail.com>
2023-03-26 14:51:00 -0300

    Ready Crud

--- src/middlewares/database.js
+++ src/middlewares/database.js
@@ -38,9 +38,27 @@ export class Database {
   } else {
     this.#database[table] = [data]
   }
-
   this.#persist();
   return data;
+  }
+
+  delete(table, id) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+    if(rowIndex > -1) {
+      this.#database[table].splice(rowIndex, 1)
+      this.#persist();
+    }
+  }
+
+  update(table, id, data) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+    if(rowIndex > -1) {
+      const row = this.#database[table][rowIndex]
+      this.#database[table][rowIndex] = {id, ...row, ...data}
+      this.#persist();
+    }
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -9,8 +9,12 @@ export const routes = [
     path    : buildRoutePath('/task'),
     handler : (req, res) => {
       const {title, description} = req.body;
+      if(!title || !description) {
+        return res.writeHead(400).end(
+          JSON.stringify({ message: 'Título e Descrição são obrigatórios'}))
+      }
       const task = {
-        id:randomUUID,
+        id:randomUUID(),
         title,
         description,
         completed_at: null,
@@ -31,6 +35,56 @@ export const routes = [
         description: search
       } : null)
       return res.end(JSON.stringify(tasks))
+    }
+  },
+  {
+    method  :'DELETE',
+    path    : buildRoutePath('/task/:id'),
+    handler : (req, res) => {
+      const { id } = req.params
+      const [task] = database.select('tasks', { id })
+      if (!task) {
+        return res.writeHead(404).end()
+      }
+      database.delete('tasks',id)
+      return res.writeHead(204).end();
+    }
+  },
+  {
+    method  : 'PUT',
+    path    : buildRoutePath('/task/:id'),
+    handler : (req, res) => {
+      const { id } = req.params
+      const {title, description } = req.body
+      if(!title || !description) {
+        return res.writeHead(400).end(
+          JSON.stringify({ message: 'Título ou Descrição são obrigatórios' }))
+      }
+      const [task] = database.select('tasks', { id })
+      if (!task) {
+        return res.writeHead(404).end()
+      }
+      database.update('tasks', id, {
+        title,
+        description,
+        updated_at : new Date()
+      })
+      return res.writeHead(204).end();
+    }
+  },
+  {
+    method: 'PATCH',
+    path: buildRoutePath('/task/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+      const [task] = database.select('tasks', { id })
+      if (!task) {
+        return res.writeHead(404).end()
+      }
+      const isTaskCompleted = !!task.completed_at
+      const completed_at = isTaskCompleted ? null : new Date()
+      database.update('tasks', id, { completed_at })
+      return res.writeHead(204).end()
     }
   }
 ]
